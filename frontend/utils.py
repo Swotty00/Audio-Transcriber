3a8a3dd9c510ac67929fbb7cdbea6c427b9680c6
devguialmeida <guilherme.almeida.data@gmail.com>
2026-05-05 15:00:16 -0300

    feat: enviar texto para o backend processar

--- frontend/utils.py
+++ frontend/utils.py
@@ -10,3 +10,16 @@ def send_to_backend(audio_bytes):
         return {"error": f"Erro no servidor: {response.status_code}"}
     except Exception as e:
         return {"error": str(e)}
+    
+
+
+def send_text_to_backend(text, relator):
+    url = "http://localhost:8000/structure-report"
+    data = {"text": text, "relator": relator}
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            return response.json()
+        return {"error": f"Erro no servidor: {response.status_code}"}
+    except Exception as e:
+        return {"error": str(e)}
